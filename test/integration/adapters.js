6cdd251478320f0b7f5c5f99ba899f3f21f6ab32
Florentin <florentins@gmail.com>
2016-05-09 15:59:49 +0300

    Testing: Add new tests.

--- test/integration/adapters.js
+++ test/integration/adapters.js
@@ -1,15 +1,14 @@
 /* eslint-env mocha */
 
 var expect = require('chai').expect
-var data = require('../referenceData')
+var refData = require('./reference-data.js')
 var runAdapters = require('./adapters-run.js')
 
 // Collecting the adapter's output.
-var collectedData = {}
+var collectedData
 
 function _collectOutput (eventName, done, eventData) {
-  // Assume now (for simplicity) that there is only one event per type.
-  collectedData[eventName] = eventData
+  collectedData.push([eventName, eventData])
   done()
 }
 
@@ -30,20 +29,96 @@ function _attachListeners (done, runner) {
 describe('Adapters integration', function () {
   Object.keys(runAdapters).forEach(function (adapter) {
     describe(adapter + ' adapter', function () {
-      var referenceData = data[adapter]
-
       before(function (done) {
-        collectedData = {}
+        collectedData = []
         runAdapters[adapter](_attachListeners.bind(null, done))
       })
 
       describe('Global suite', function () {
+        it('should be emitted on runStart event', function () {
+          expect(collectedData[0][0]).to.be.equal('runStart')
+        })
+
         it('should have no name', function () {
-          expect(referenceData[0][1].name).to.be.equal(collectedData.runStart.name)
+          expect(collectedData[0][1].name).to.be.equal(refData[0][1].name)
         })
 
         it('should have all the other suites as childSuites', function () {
-          expect(referenceData[0][1].childSuites).to.have.lengthOf(collectedData.runStart.childSuites.length)
+          expect(collectedData[0][1].childSuites).to.have
+            .lengthOf(refData[0][1].childSuites.length)
+        })
+      })
+
+      describe('Suite with only one passing test', function () {
+        it('should be emitted on suiteStart event', function () {
+          expect(collectedData[1][0]).to.be.equal('suiteStart')
+        })
+
+        it('should have a name', function () {
+          expect(collectedData[1][1].suiteName).to.be
+            .equal(refData[1][1].suiteName)
+        })
+
+        it('should have no other child suites', function () {
+          expect(collectedData[1][1].childSuites).to.have
+            .lengthOf(refData[1][1].childSuites.length)
+        })
+
+        it('should contain only one test', function () {
+          expect(collectedData[1][1].tests).to.have
+            .lengthOf(refData[1][1].tests.length)
+        })
+
+        describe('Test on start', function () {
+          it('should have been emitted on testStart event', function () {
+            expect(collectedData[2][0]).to.be.equal('testStart')
+          })
+
+          it('should have a name', function () {
+            expect(collectedData[2][1].testName).to.be
+              .equal(refData[2][1].testName)
+          })
+
+          it('should contain the name of the suite parent', function () {
+            expect(collectedData[2][1].suiteName).to.be
+              .equal(refData[2][1].suiteName)
+          })
+
+          it('should have only 2 props: testName and suiteName', function () {
+            expect(collectedData[2][1]).to.have.all
+              .keys(['testName', 'suiteName'])
+          })
+        })
+
+        describe('Test on end', function () {
+          it('should have been emitted on testEnd event', function () {
+            expect(collectedData[3][0]).to.be.equal('testEnd')
+          })
+
+          it('should have the same name as on start', function () {
+            expect(collectedData[3][1].testName).to.be
+              .equal(refData[3][1].testName)
+          })
+
+          it('should contain the name of the suite parent', function () {
+            expect(collectedData[3][1].suiteName).to.be
+              .equal(refData[3][1].suiteName)
+          })
+
+          it('should contain status as passed', function () {
+            expect(collectedData[3][1].status).to.be
+              .equal(refData[3][1].status)
+          })
+
+          it('should contain its runtime', function () {
+            expect(collectedData[3][1].runtime).to.be
+              .closeTo(refData[3][1].runtime, 1)
+          })
+
+          it('should have no errors', function () {
+            expect(collectedData[3][1].errors).to.be.deep
+              .equal(refData[3][1].errors)
+          })
         })
       })
     })
